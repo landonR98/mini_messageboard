c7f159cb1ca448e39aaf4f14734fa20db8f89b8c
Landon Ryan <landonr98@gmail.com>
2024-03-05 15:54:59 -0330

    index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+/** @type {Array<{text:string,user:string,added:Date}>} */
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+]
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Messages", posts: messages });
 });
+
+router.get("/new", (req, res, next) => {
+  res.render("index", { title: "New Message" });
+});
+
+router.post("/new", (req, res, next) =>{
+  res.redirect("/")
+})
 
 module.exports = router;
