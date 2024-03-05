c797bc26628be0fcb1dd133772b6b8993d21098c
Landon Ryan <landonr98@gmail.com>
2024-03-05 13:01:19 -0330

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": false,
+        "node": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "no-unused-vars": 0
+    }
+}
