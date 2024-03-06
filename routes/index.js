6580147d4fe0e25acd6f77a8cee95f69de691ee1
Landon Ryan <landonr98@gmail.com>
2024-03-05 21:57:39 -0330

    title change

--- routes/index.js
+++ routes/index.js
@@ -6,45 +6,48 @@ const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: new Date(),
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
-  }
-]
+    added: new Date(),
+  },
+];
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Messages", posts: messages });
+  res.render("index", { title: "Mini Messageboard", posts: messages });
 });
 
 router.get("/new", (req, res, next) => {
   res.render("form", { title: "New Message" });
 });
 
-router.post("/new", (req, res, next) =>{
+router.post("/new", (req, res, next) => {
   /** @type {string} */
-  let user = (req.body.user? req.body.user: "").trim()
-  if (user === ""){
-    user = "Anonymous"
+  let user = (req.body.user ? req.body.user : "").trim();
+  if (user === "") {
+    user = "Anonymous";
   }
 
   /** @type {string} */
-  const messageText = (req.body.messageText? req.body.messageText: "").trim()
-  if (messageText === ""){
-    res.render("form", {title: "New Message", user: user, error:"Message must not be empty"})
-
-  }else{
+  const messageText = (req.body.messageText ? req.body.messageText : "").trim();
+  if (messageText === "") {
+    res.render("form", {
+      title: "New Message",
+      user: user,
+      error: "Message must not be empty",
+    });
+  } else {
     messages.push({
       text: messageText,
       user: user,
-      added: new Date()
-    })
-    
-    res.redirect("/")
+      added: new Date(),
+    });
+
+    res.redirect("/");
   }
-})
+});
 
 module.exports = router;
